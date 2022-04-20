4b28a05d96275fdf375752e5f96572511861e333
dshayderov <dshayderov@list.ru>
2022-04-20 21:47:42 +0300

    Добавлен выбор действия

--- pr2/pr2/pr2.cpp
+++ pr2/pr2/pr2.cpp
@@ -11,17 +11,32 @@ int main()
 	cout << "Project name: ";
 	cin >> b;
 	cout << "\n" << b << " by " << a << endl;
-	int sl1, sl2, sub1, sub2, mul1, mul2, div1, div2;
-	cout << "Addition\n" << "Enter sl1 and sl2:";
-	cin >> sl1 >> sl2;
-	cout << "Result: " << sl1 + sl2 << endl;
-	cout << "Subtraction\n" << "Enter sub1 and sub2: ";
-	cin >> sub1 >> sub2;
-	cout << "Result: " << sub1 - sub2 << endl;
-	cout << "Multiplicationn\n" << "Enter mul1 and mul2: ";
-	cin >> mul1 >> mul2;
-	cout << "Result: " << mul1*mul2 << endl;
-	cout << "Division\n" << "Enter div1 and div2: ";
-	cin >> div1 >> div2;
-	cout << "Result: " << div1 /div2 << endl;
+	int sl1, sl2, sub1, sub2, mul1, mul2, div1, div2, ch;
+	cout << "Choose the action: \n1-Addition\n2-Subtraction\n3-Multiplication\n4-Division\n";
+	cin >> ch;
+	if (ch == 1)
+	{
+		cout << "Addition\n" << "Enter sl1 and sl2:";
+		cin >> sl1 >> sl2;
+		cout << "Result: " << sl1 + sl2 << endl;
+	}
+	if (ch == 2)
+	{
+		cout << "Subtraction\n" << "Enter sub1 and sub2: ";
+		cin >> sub1 >> sub2;
+		cout << "Result: " << sub1 - sub2 << endl;
+	}
+	if (ch == 3)
+	{
+		cout << "Multiplicationn\n" << "Enter mul1 and mul2: ";
+		cin >> mul1 >> mul2;
+		cout << "Result: " << mul1 * mul2 << endl;
+	}
+	if (ch == 4)
+	{
+		cout << "Division\n" << "Enter div1 and div2: ";
+		cin >> div1 >> div2;
+		cout << "Result: " << div1 / div2 << endl;
+	}
+	return 0;
 }

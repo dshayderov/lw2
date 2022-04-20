16b069d6578b6fcda59b508f4def0507fc237bf8
dshayderov <dshayderov@list.ru>
2022-04-20 21:07:10 +0300

    Новый проект

---
+++ pr2/pr2/pr2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	string a, b;
+	cout << "Hello!\n";
+	cout << "Your name: ";
+	cin >> a;
+	cout << "Project name: ";
+	cin >> b;
+	cout << "\n" << b << " by " << a << endl;
+}

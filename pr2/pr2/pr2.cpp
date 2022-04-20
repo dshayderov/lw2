54a0cb9e213394a58543c020c36b50b2f2fed2d7
dshayderov <dshayderov@list.ru>
2022-04-20 21:35:59 +0300

    Добавлено умножение

--- pr2/pr2/pr2.cpp
+++ pr2/pr2/pr2.cpp
@@ -11,11 +11,14 @@ int main()
 	cout << "Project name: ";
 	cin >> b;
 	cout << "\n" << b << " by " << a << endl;
-	int sl1, sl2, sub1, sub2;
+	int sl1, sl2, sub1, sub2, mul1, mul2;
 	cout << "Addition\n" << "Enter sl1 and sl2:";
 	cin >> sl1 >> sl2;
 	cout << "Result: " << sl1 + sl2 << endl;
 	cout << "Subtraction\n" << "Enter sub1 and sub2: ";
 	cin >> sub1 >> sub2;
 	cout << "Result: " << sub1 - sub2 << endl;
+	cout << "Multiplicationn\n" << "Enter mul1 and mul2: ";
+	cin >> mul1 >> mul2;
+	cout << "Result: " << mul1*mul2 << endl;
 }
